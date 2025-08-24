e1f93d586df50b2e32de320b8bf849d0aacb3281
Svyat <svyat.kostevich@gmail.com>
2025-08-24 20:00:10 +0200

    models(user): added AbstractUser

---
+++ social_network_be/user/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=100, unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username", "first_name"]
+
+    def __str__(self):
+        return self.username
+
+
+
+
+
+
+
