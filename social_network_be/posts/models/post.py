8596f70ec79bfc8b3689ed6130eaa85c5f8ee1db
Svyat <svyat.kostevich@gmail.com>
2025-11-07 17:59:23 +0100

    feat(model): Remade Model for posts

--- social_network_be/posts/models/post.py
+++ social_network_be/posts/models/post.py
@@ -3,24 +3,18 @@ from django.db import models
 from .components import Comments
 
 from user.models import User
+from base.models import TimeStampedModel
 
-
-class BasePostModel(models.Model):
+class PostModel(TimeStampedModel, models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
+    image = models.ImageField(upload_to='posts/', null=True, blank=True)
+
     likes = models.IntegerField(null=True, blank=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comments = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
 
-    class Meta:
-        abstract = True
 
-
-class PostPhotoModel(BasePostModel):
-    image = models.ImageField(upload_to='posts/', null=False)
-
-class PostTextModel(BasePostModel):
-    pass
 
 
