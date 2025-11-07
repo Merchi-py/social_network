57d4e0f5956f78b1165d6edf624928cd2eec11d5
Svyat <svyat.kostevich@gmail.com>
2025-11-07 18:02:29 +0100

    feat(admin): Remade Model for posts

--- social_network_be/posts/admin.py
+++ social_network_be/posts/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import PostTextModel, PostPhotoModel, Comments
+from .models import PostModel, Comments
 from user import models
 
 
-admin.site.register(PostPhotoModel)
-admin.site.register(PostTextModel)
+admin.site.register(PostModel)
 admin.site.register(Comments)
 admin.site.register(models.User)
