71c88ddbbb4646db17b0e379d5285c9ed2bf66a3
Svyat <svyat.kostevich@gmail.com>
2025-11-12 20:24:56 +0100

    feat(managers.py): added sort by Posts

---
+++ social_network_be/posts/managers.py
@@ -0,0 +1,23 @@
+from django.db import models
+from datetime import datetime, timedelta
+
+
+class PostQuerySet(models.QuerySet):
+    def get_new_posts(self):
+        two_days_ago = datetime.now() - timedelta(days=2)
+        return self.filter(created_at__gt=two_days_ago)
+
+    def get_sorted_posts(self):
+        return self.get_new_posts().order_by("-created_at")
+
+
+class PostManager(models.Manager):
+
+    def get_queryset(self):
+        return PostQuerySet(self.model, using=self._db)
+
+    def get_new_posts(self):
+        return self.get_queryset().get_new_posts()
+
+    def get_sorted_posts(self):
+        return self.get_queryset().get_sorted_posts()
