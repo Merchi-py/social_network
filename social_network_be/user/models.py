17e7b1faeefcc855b0e13c8ab6d630f0b6a296ed
Svyat <svyat.kostevich@gmail.com>
2025-11-12 18:25:58 +0100

    none(models.py): No changes

--- social_network_be/user/models.py
+++ social_network_be/user/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .managers import *
+
 class User(AbstractUser):
-    def __str__(self):
-        self.username
+    pass
+
