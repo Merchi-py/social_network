e7ac8ff1c2c95a0a1b202d4d70ff80914e9e031e
Svyat <svyat.kostevich@gmail.com>
2025-11-12 18:25:10 +0100

    feat(post.py): added new Manager objects

--- social_network_be/posts/models/post.py
+++ social_network_be/posts/models/post.py
@@ -3,7 +3,9 @@ from django.db import models
 from .components import Comments
 
 from user.models import User
+from user.managers import SortedProfilePosts
 from base.models import TimeStampedModel
+
 
 class PostModel(TimeStampedModel, models.Model):
     title = models.CharField(max_length=50)
@@ -14,6 +16,8 @@ class PostModel(TimeStampedModel, models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comments = models.ForeignKey(Comments, on_delete=models.CASCADE, blank=True, null=True)
+
+    objects = SortedProfilePosts()
 
 
 
