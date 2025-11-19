7ebda093187cde2acc5229a389047132db9a7a8c
Svyat <svyat.kostevich@gmail.com>
2025-11-19 19:06:23 +0100

    feat(views): added Posts deleating

--- social_network_be/posts/views.py
+++ social_network_be/posts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.views.generic import ListView, TemplateView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 from .models import PostModel
 from .forms import PostCreationForm
@@ -12,14 +13,17 @@ class PostView(ListView):
     model = PostModel
     context_object_name = "text_posts"
 
+
     def get_queryset(self):
         return PostModel.objects.get_sorted_posts()
+
 
 class PostCreateView(CreateView, LoginRequiredMixin):
     template_name = "posts/post_create.html"
     model = PostModel
     form_class = PostCreationForm
-    success_url = "/"
+    success_url = reverse_lazy('home')
+
 
     def form_valid(self, form):
         if self.request.user.is_authenticated:
@@ -30,4 +34,16 @@ class PostCreateView(CreateView, LoginRequiredMixin):
 class PostDeleteView(DeleteView, LoginRequiredMixin):
     model = PostModel
     http_method_names = ['post']
-    success_url = '/'
+    success_url = reverse_lazy('home')
+
+
+class PostDetailView(DetailView, LoginRequiredMixin):
+    template_name = 'posts/post_detail.html'
+    model = PostModel
+    slug_field = 'id'
+    slug_url_kwarg = 'post_id'
+    context_object_name = 'post'
+
+# class PostUpdateView(UpdateView, LoginRequiredMixin):
+#     template_name = "posts/post_update.html"
+#     model = PostModel
