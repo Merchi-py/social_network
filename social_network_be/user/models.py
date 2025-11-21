e32892474372ea751c98a1b2973f22aceefb1b3f
Svyat <svyat.kostevich@gmail.com>
2025-11-21 18:24:48 +0100

    feat(models): added image column to load avatar

--- social_network_be/user/models.py
+++ social_network_be/user/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    avatar = models.ImageField(upload_to='user/', null=True, blank=True)
 
