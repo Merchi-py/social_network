ea16be0cf120e1ac1bb99512cbef7c86c672a024
Svyat <svyat.kostevich@gmail.com>
2025-11-21 17:07:14 +0100

    feat(forms): added form for UpdateView

--- social_network_be/posts/forms.py
+++ social_network_be/posts/forms.py
@@ -17,3 +17,19 @@ class PostCreationForm(forms.ModelForm):
                 "class": "form_control"
             }),
         }
+
+class PostUpdateForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields = ["title", "content"]
+
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "placeholder": "Enter the title",
+                "class": "form_control"
+            }),
+            "content": forms.Textarea(attrs={
+                "placeholder": "Enter the content",
+                "class": "form_control"
+            }),
+        }
