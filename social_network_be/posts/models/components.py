1fe27115b1f59e031ae725a17448c27025543310
Svyat <svyat.kostevich@gmail.com>
2025-11-26 17:58:13 +0100

    feat(component): added Rating Model

--- social_network_be/posts/models/components.py
+++ social_network_be/posts/models/components.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import User
+from .post import PostModel
 
 
 class Comments(models.Model):
@@ -7,3 +8,18 @@ class Comments(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+class Rating(models.Model):
+    LIKE = 'like'
+    DISLIKE = 'dislike'
+
+    RATING = [
+        (LIKE, 'like'),
+        (DISLIKE, 'dislike')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostModel, on_delete=models.CASCADE)
+    reaction = models.CharField(max_length=7, choices=RATING)
+
+    class Meta:
+        unique_together = ("user", "post")
