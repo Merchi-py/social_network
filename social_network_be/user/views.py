9e264e80e1af505b7dabeb60e93f003a3ef1c10d
Svyat <svyat.kostevich@gmail.com>
2025-11-26 20:19:56 +0100

    feat(views): added ProfileEdit for user

--- social_network_be/user/views.py
+++ social_network_be/user/views.py
@@ -1,9 +1,11 @@
-from django.views.generic import ListView, TemplateView, DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from posts.models import PostModel
-
+from .models import User
+from .forms import UserUpdateForm
 
 class Profile(ListView, LoginRequiredMixin):
     template_name = "user/profile.html"
@@ -12,3 +14,13 @@ class Profile(ListView, LoginRequiredMixin):
 
     def get_queryset(self):
         return PostModel.profile.get_sorted_posts(self.request.user)
+
+class ProfileEdit(UpdateView, LoginRequiredMixin):
+    template_name = "user/profile_edit.html"
+    model = User
+    slug_field = 'id'
+    slug_url_kwarg = 'user_id'
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("profile")
+
+
