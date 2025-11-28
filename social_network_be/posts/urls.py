c87e9305fed287952b1f7d4ee6b1669ec58e598f
Svyat <svyat.kostevich@gmail.com>
2025-11-28 17:54:03 +0100

    feat(urls): added url for comment create

--- social_network_be/posts/urls.py
+++ social_network_be/posts/urls.py
@@ -19,11 +19,13 @@ from .views import *
 
 urlpatterns = [
     path("", PostView.as_view(), name="home"),
-    path("create-post/", PostCreateView.as_view(), name="create_text"),
+    path("create-post/", PostCreateView.as_view(), name="create_post"),
     path("delete-post/<int:pk>/", PostDeleteView.as_view(), name="delete_post"),
     path("detail-post/<int:post_id>/", PostDetailView.as_view(), name="detail_post"),
     path("update-post/<int:post_id>/", PostUpdateView.as_view(), name="update_post"),
     path("like_system/<int:post_id>/", LikeSystem.as_view(), name="like_system"),
     path("dislike_system/<int:post_id>/", DisLikeSystem.as_view(), name="dislike_system"),
+    path("create-comment/<int:post_id>/", CommentCreate.as_view(), name="create_comment")
+
 ]
 
